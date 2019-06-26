d08baa437ea057cd05f0406b78a603b19c50499b
Josh Gibson <flash50k@gmail.com>
2019-06-26 14:23:07 +0100

    done second method

---
+++ my_functions.rb
@@ -0,0 +1,18 @@
+
+# array1 = ["1","2","3"]
+# array2 = [5,6,7,8]
+
+def add_array_lengths(array1, array2)
+return array1.length + array2.length
+end
+
+# p add_length_of_arrays(array1, array2)
+
+
+def sum_array(numbers)
+total = 0
+  for number in numbers
+    total += number
+  end
+return total
+end
